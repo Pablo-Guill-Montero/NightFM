cd20bb6ebe422081ef41647bc81017cd56bfbb4f
Pablo Guill Montero <pabloguillmontero@gmail.com>
2026-03-04 16:11:41 +0100

    ResoSelector

---
+++ Assets/Scripts/ResoSelector.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using TMPro;
+
+public class ResoSelector : MonoBehaviour
+{
+    public TextMeshProUGUI textoResolucion;
+    private Resolution[] resoluciones;
+    private int indiceActual = 0;
+
+    void Start()
+    {
+        // Obtiene las resoluciones disponibles del monitor
+        resoluciones = Screen.resolutions;
+        // Busca cuál es la resolución actual para empezar desde ahí
+        for (int i = 0; i < resoluciones.Length; i++) {
+            if (resoluciones[i].width == Screen.currentResolution.width && 
+                resoluciones[i].height == Screen.currentResolution.height) {
+                indiceActual = i;
+            }
+        }
+        ActualizarInterfaz();
+    }
+
+    public void Siguiente() {
+        indiceActual = (indiceActual + 1) % resoluciones.Length;
+        AplicarYActualizar();
+    }
+
+    public void Anterior() {
+        indiceActual--;
+        if (indiceActual < 0) indiceActual = resoluciones.Length - 1;
+        AplicarYActualizar();
+    }
+
+    void AplicarYActualizar() {
+        Resolution res = resoluciones[indiceActual];
+        Screen.SetResolution(res.width, res.height, Screen.fullScreen);
+        ActualizarInterfaz();
+    }
+
+    void ActualizarInterfaz() {
+        textoResolucion.text = resoluciones[indiceActual].width + " x " + resoluciones[indiceActual].height;
+    }
+}
